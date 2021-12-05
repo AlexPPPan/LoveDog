b1ee64447cdae1e51c41f149e035f60e526abbb2
alex82549250 <alex82549250@163.com>
2021-12-05 22:16:17 +0800

    chenge Theme

---
+++ app/src/main/java/com/homework/lovedog/activity/SplashActivity.java
@@ -0,0 +1,21 @@
+package com.homework.lovedog.activity;
+
+import android.view.View;
+
+import com.homework.lovedog.base.BaseActivity;
+
+/**
+ * @author tianbin.
+ */
+
+class SplashActivity extends BaseActivity {
+    @Override
+    protected int fragmentLayoutId() {
+        return 0;
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
