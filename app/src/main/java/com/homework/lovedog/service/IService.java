00b3c1a376e2f0649d88e9f7a5dbbdb0be9d24ec
alex82549250 <alex82549250@163.com>
2021-12-09 09:29:17 +0800

    develop main

---
+++ app/src/main/java/com/homework/lovedog/service/IService.java
@@ -0,0 +1,10 @@
+package com.homework.lovedog.service;
+
+/**
+ * @author tianbin.
+ */
+
+interface IService {
+
+    String DOG_API = "https://api.apishop.net/common/dogFamily/queryDogListByKeyword";
+}
